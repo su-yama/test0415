e11de7dc68615a8e93041fda5974c9fce38b501a
speed-of-sound <guardianforce.767@gmail.com>
2016-05-05 16:05:06 +0900

    add new project

---
+++ Appointment/app/src/main/java/appointment/test/co/jp/appointment/MainActivity.java
@@ -0,0 +1,13 @@
+package appointment.test.co.jp.appointment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
